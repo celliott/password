62c015059f4b91abaa0dcbd4004f558506c4060d
Chris Elliott <ctelliott@gmail.com>
2017-11-20 12:44:19 -0500

    Updated check

--- docker/auth-proxy/docker-entrypoint.sh
+++ docker/auth-proxy/docker-entrypoint.sh
@@ -3,8 +3,7 @@ set -e
 
 AUTH_USER=${AUTH_USER:-'admin'}
 if [ -z "$AUTH_PASS" ]; then
-  echo "AUTH_PASS must be set"
-  exit 10
+  echo "AUTH_PASS must be set"; exit 1
 fi
 
 htpasswd -b -c /etc/nginx/.htpasswd $AUTH_USER $AUTH_PASS
